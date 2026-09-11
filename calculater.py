aa42d9de6ce423daad50340a7e026dcd7c478c4b
binitthakur86-code <binitthakur86@gmail.com>
2026-09-11 16:08:15 +0530

    Create calculator.py

---
+++ calculater.py
@@ -0,0 +1,10 @@
+# My first Python project - Class 11
+print("Welcome to my calculator!")
+
+a = int(input("First number: "))
+b = int(input("Second number: "))
+
+print(f"Sum: {a+b}")
+print(f"Minus: {a-b}")
+print(f"Multiply: {a*b}")
+print("Made by Binit from madhubani")
